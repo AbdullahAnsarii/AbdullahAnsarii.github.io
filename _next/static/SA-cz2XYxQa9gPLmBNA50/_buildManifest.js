fed12659f43b376addf38baab73c564544acb95c
Abdullah Ansari <aa9272137@gmail.com>
2023-12-03 03:16:21 +0400

    added dat project and oxinus

--- _next/static/SA-cz2XYxQa9gPLmBNA50/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/72b11be3176dc168.css","static/chunks/pages/index-fd53d6b17debd1f9.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
