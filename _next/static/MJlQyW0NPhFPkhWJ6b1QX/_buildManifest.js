464255637d07d75a635df5b404d2705dd740febd
Abdullah Ansari <aa9272137@gmail.com>
2023-12-11 17:10:29 +0400

    added new skill and updated Next.js Developer to Node.js Developer

--- _next/static/MJlQyW0NPhFPkhWJ6b1QX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/72b11be3176dc168.css","static/chunks/pages/index-17ce806f74b11411.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/mOjC45igHmynvATLKqIx0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5aa2b26db3e90cc9.css","static/chunks/pages/index-95c1aecd01957cfb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
